74a43ad5cd540ff2ffa9949c36d839e31444e268
vumanhtien <tienvmgch17404@fpt.edu.vn>
2019-08-29 16:21:17 +0700

    sada

--- delete.php
+++ delete.php
@@ -1,7 +1,7 @@
 <?php
 require("connect.php");
 $id = $_POST['stt'];
-$sql = "DELETE FROM product WHERE stt = '$stt'";
+$sql = "DELETE FROM toys WHERE stt = '$stt'";
 pg_query($conn,$sql); 
 header("Location: sanpham.php");
 ?>
